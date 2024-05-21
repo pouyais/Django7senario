790a95abe65fcdecc76f1e314dd193da8a237da5
pouyais <157633426+pouyais@users.noreply.github.com>
2024-05-21 20:59:39 +0330

    ok

--- root/views.py
+++ root/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Services,Skill,Team,Portfolio
 
 
 def index(request):
-    return render(request,'root/index.html')
+    context = {
+        'services' : Services.objects.filter(status=True),
+        'skills' : Skill.objects.filter(status=True),
+        'teams' : Team.objects.filter(status=True),
+        'portfolios' : Portfolio.objects.filter(status=True),
+        'servicescount' : Services.objects.all().count(),
+        'skillscount' : Skill.objects.all().count(),
+        'teamscount' : Team.objects.all().count(),
+        'portfolioscount' : Portfolio.objects.all().count(),
+    }
+    return render(request,'root/index.html',context=context)
